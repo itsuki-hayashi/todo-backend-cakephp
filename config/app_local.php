e7c106ddb74c814da58a1a2edb28febcbe12b2fb
zhiyuan-lin <2121340+zhiyuan-lin@users.noreply.github.com>
2019-12-24 18:07:29 +0900

    Add app_local.php

---
+++ config/app_local.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Local configuration file to provide any overrides to your app.php configuration.
+ * Copy and save this file as app_local.php and make changes as required.
+ * Note: It is not recommended to commit files with credentials such as app_local.php
+ * into source code version control.
+ */
+return [
+    /*
+     * Debug Level:
+     *
+     * Production Mode:
+     * false: No error messages, errors, or warnings shown.
+     *
+     * Development Mode:
+     * true: Errors and warnings shown.
+     */
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+
+    /*
+     * Security and encryption configuration
+     *
+     * - salt - A random string used in security hashing methods.
+     *   The salt value is also used as the encryption key.
+     *   You should treat it as extremely sensitive data.
+     */
+    'Security' => [
+        'salt' => env('SECURITY_SALT', 'c01bff4b5f041d07fd331ad6534ec5035431b8d16f3a30060c8ea567599afced'),
+    ],
+
+    /*
+     * Connection information used by the ORM to connect
+     * to your application's datastores.
+     *
+     * See app.php for more configuration options.
+     */
+    'Datasources' => [
+        'default' => [
+            /**
+             * You can use a DSN string to set the entire configuration
+             */
+            'url' => getenv("CLEARDB_DATABASE_URL"),
+            'quoteIdentifiers' => true
+        ]
+    ]
+];
